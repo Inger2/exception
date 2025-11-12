9251986b53352f320de3f771047d93c480bad87a
Inger2 <oleg.sidor2121@gmail.com>
2025-11-12 15:54:19 +0200

    Update ExceptionsString.java

--- ExceptionsString.java
+++ ExceptionsString.java
@@ -1,14 +1,12 @@
 package exceptions;
 
-import java.util.Arrays;
-
 public class ExceptionsString {
     public static void main(String[] args) throws InvalidSignException,
             FloatNumbersException,
             TooManyArgumentsException {
-        String equation = "34+435-20=449+20-19";
+        String equation = "5*2-2*3=4";
         StringCalculator cs = new StringCalculator();
-        System.out.println(cs.stringCompare(equation));
+        System.out.println(cs.compareNumbers(equation));
 
     }
 }
@@ -21,20 +19,115 @@ class StringCalculator {
         return equation.split(splitRegex);
     }
 
-    boolean stringCompare(String equation) throws InvalidSignException,
+    int evaluateExpression(String equation) throws InvalidSignException,
+            FloatNumbersException,
+            TooManyArgumentsException {
+        int temp = 0;
+        int result = 0;
+        String operator = "+";
+        String[] equationSplit = equation.split("(?=[/+\\-*])|(?<=[/+\\-*])");
+        int count = equationSplit.length;
+        checkAllExceptions(equation);
+        for (int i = 0; i < equationSplit.length; i++) {
+            if (equationSplit[i].matches("[/+\\-*]") && equationSplit[i + 1].matches("-")) {
+                equationSplit[i + 2] = equationSplit[i + 1] + equationSplit[i + 2];
+                equationSplit[i + 1] = "";
+
+            }
+        }
+
+        for (int i = 0; i < equationSplit.length; i++) {
+            if (equationSplit[i].isEmpty()) {
+                count--;
+            }
+        }
+        String[] secondEquationSplit = new String[count];
+        int index = 0;
+        for (int i = 0; i < equationSplit.length; i++) {
+            if (!equationSplit[i].isEmpty()) {
+                secondEquationSplit[index] = equationSplit[i];
+                index++;
+            }
+        }
+        int left;
+        int right;
+
+        for (int i = 0; i < secondEquationSplit.length; i++) {
+            if (secondEquationSplit[i].equals("*") || secondEquationSplit[i].equals("/")) {
+                left = Integer.parseInt(secondEquationSplit[i - 1]);
+                right = Integer.parseInt(secondEquationSplit[i + 1]);
+                if (secondEquationSplit[i].equals("*")) {
+                    temp = left * right;
+                    secondEquationSplit[i + 1] = String.valueOf(temp);
+                    secondEquationSplit[i] = "";
+                    secondEquationSplit[i - 1] = "";
+                } else if (secondEquationSplit[i].equals("/")) {
+                    temp = left / right;
+                    secondEquationSplit[i + 1] = String.valueOf(temp);
+                    secondEquationSplit[i] = "";
+                    secondEquationSplit[i - 1] = "";
+                }
+            }
+        }
+        int secondCount = secondEquationSplit.length;
+        for (int i = 0; i < secondEquationSplit.length; i++) {
+            if (secondEquationSplit[i].isEmpty()) {
+                secondCount--;
+            }
+
+        }
+        String[] newEquationSplit = new String[secondCount];
+        int secondIndex = 0;
+        for (int i = 0; i < secondEquationSplit.length; i++) {
+            if (!secondEquationSplit[i].isEmpty()) {
+                newEquationSplit[secondIndex] = secondEquationSplit[i];
+                secondIndex++;
+            }
+        }
+
+        // массив с пустыми строками -> считаю количество пустых строк -> созадю новый массив поменьше -> переношу в него все кроме пустых строк
+
+        for (String equationCalc : newEquationSplit) {
+
+
+            switch (equationCalc) {
+                case "+":
+                    operator = "+";
+                    break;
+                case "-":
+                    operator = "-";
+                    break;
+            }
+            if (!equationCalc.matches("-?\\d+")) {
+
+                equationCalc = equationCalc.replaceAll("[+-]", "");
+                if (equationCalc.isEmpty()) {
+                    continue;
+                }
+            }
+            int equationNumber = Integer.parseInt(equationCalc);
+            switch (operator) {
+                case "+":
+                    result += equationNumber;
+                    break;
+                case "-":
+                    result -= equationNumber;
+                    break;
+            }
+
+        }
+        return result;
+    }
+
+
+    boolean compareNumbers(String equation) throws InvalidSignException,
             FloatNumbersException,
             TooManyArgumentsException {
         String[] equationArray = splitEquation(equation);
-        String leftPart = equationArray[0];
-        String rightPart = equationArray[1];
-        String[] leftPartArray = leftPart.replaceAll("\\s", "").split("\\+|(?=-)");
-        String[] rightPartArray = rightPart.replaceAll("\\s", "").split("\\+|(?=-)");
-        checkAllExceptions(equation);
-        return Arrays
-                .stream(leftPartArray)
-                .mapToInt(Integer::parseInt).sum() == Arrays
-                .stream(rightPartArray)
-                .mapToInt(Integer::parseInt).sum();
+        int leftPart = evaluateExpression(equationArray[0]);
+        int rightPart = evaluateExpression(equationArray[1]);
+        return leftPart == rightPart;
+
     }
 
     void validateSign(String equation) throws InvalidSignException {
@@ -50,12 +143,15 @@ class StringCalculator {
     }
 
     void validateOperandCount(String equation) throws TooManyArgumentsException {
-        String[] arrayEquation = splitEquation(equation);
-        String splitRegex = "[-+*/]";
-        String[] leftPart = arrayEquation[0].split(splitRegex);
-        String[] rightPart = arrayEquation[1].split(splitRegex);
-        if (leftPart.length > MAX_OPERAND_NUMBER || rightPart.length > MAX_OPERAND_NUMBER) {
-            throw new TooManyArgumentsException("Слишком длинное выражение");
+        String[] equationSplit = equation.split("(?=[+\\-*/])|(?<=[+\\-*/])");
+        int count = 0;
+        for (String s : equationSplit) {
+            if (s.matches("\\d+$")) {
+                count++;
+            }
+            if (count > MAX_OPERAND_NUMBER) {
+                throw new TooManyArgumentsException("Слишком Длинное выражение");
+            }
         }
     }
 
